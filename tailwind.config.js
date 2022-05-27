448714e96ef480207001af87bcd78d1d8752f6ef
hsihak <sihak.hs@gmail.com>
2022-05-27 04:02:24 -0400

    Completed mobile design light-mode

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      darkMode: 'class',
+      colors: {
+        // Primary
+        'facebook': 'hsl(208, 92%, 53%)',
+        'twitter': 'hsl(203, 89%, 53%)',
+        'instagram': 'rgb(253,196,104)',
+        'youtube': 'hsl(348, 97%, 39%)',
+        // Light Theme
+        'lime-green': 'hsl(163, 72%, 41%)',
+        'bright-red': 'hsl(356, 69%, 56%)',
+        'white': 'hsl(0, 0%, 100%)',
+        'very-pale-blue': 'hsl(225, 100%, 98%)',
+        'light-grayish-blue': 'hsl(227, 47%, 96%)',
+        'dark-grayish-blue': 'hsl(228, 12%, 44%)',
+        'very-dark-blue': 'hsl(230, 17%, 14%)',
+//         #### Dark Theme
+        'very-dark-blue': 'hsl(230, 17%, 14%)',
+        'very-dark-blue-pattern': 'hsl(232, 19%, 15%)',
+        'dark-desaturated-blue': 'hsl(228, 28%, 20%)',
+        'desaturated-blue': 'hsl(228, 34%, 66%)',
+        'white': 'hsl(0, 0%, 100%)'
+      }
+    },
+  },
+  plugins: [],
+}
