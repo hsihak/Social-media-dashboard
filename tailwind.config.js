6dacf319a993ec02bcc9bbea4a07303cdc53732f
hsihak <sihak.hs@gmail.com>
2022-05-27 21:07:06 -0400

    Completed desktop design light and dark-mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,12 +17,14 @@ module.exports = {
         'light-grayish-blue': 'hsl(227, 47%, 96%)',
         'dark-grayish-blue': 'hsl(228, 12%, 44%)',
         'very-dark-blue': 'hsl(230, 17%, 14%)',
+        'toggle-light': 'hsl(230, 22%, 74%)',
 //         #### Dark Theme
         'very-dark-blue': 'hsl(230, 17%, 14%)',
         'very-dark-blue-pattern': 'hsl(232, 19%, 15%)',
         'dark-desaturated-blue': 'hsl(228, 28%, 20%)',
         'desaturated-blue': 'hsl(228, 34%, 66%)',
-        'white': 'hsl(0, 0%, 100%)'
+        'white': 'hsl(0, 0%, 100%)',
+        'toggle-dark': 'rgba(240,246,252,0.1)'
       }
     },
   },
